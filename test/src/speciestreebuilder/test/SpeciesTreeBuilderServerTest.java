024ea9ee1354d3f6041f8394643eff85c8dc515f
JamesJeffryes <jamesgjeffryes@gmail.com>
2018-08-13 15:16:52 -0500

    Use RefSeq genome rather than loading one

--- test/src/speciestreebuilder/test/SpeciesTreeBuilderServerTest.java
+++ test/src/speciestreebuilder/test/SpeciesTreeBuilderServerTest.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,13 +17,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import buildtestgenome.BuildTestGenomeClient;
-import buildtestgenome.PrepareTestGenomeFromProteinsParams;
-
-import speciestreebuilder.BuildGenomeSetFromTreeParams;
 import speciestreebuilder.ConstructSpeciesTreeParams;
-import speciestreebuilder.FindCloseGenomesParams;
-import speciestreebuilder.GuessTaxonomyPathParams;
 import speciestreebuilder.SpeciesTreeBuilderServer;
 import us.kbase.auth.AuthConfig;
 import us.kbase.auth.AuthToken;
@@ -34,17 +26,16 @@ import us.kbase.auth.ConfigurableAuthService;
 import us.kbase.common.service.JsonServerSyslog;
 import us.kbase.common.service.RpcContext;
 import us.kbase.common.service.UObject;
-import us.kbase.common.utils.FastaReader;
 import us.kbase.kbasetrees.Tree;
-import us.kbase.workspace.CreateWorkspaceParams;
-import us.kbase.workspace.GetObjects2Params;
-import us.kbase.workspace.ObjectData;
-import us.kbase.workspace.ObjectSaveData;
-import us.kbase.workspace.ObjectSpecification;
-import us.kbase.workspace.ProvenanceAction;
-import us.kbase.workspace.SaveObjectsParams;
-import us.kbase.workspace.WorkspaceClient;
-import us.kbase.workspace.WorkspaceIdentity;
+import workspace.CreateWorkspaceParams;
+import workspace.GetObjects2Params;
+import workspace.ObjectData;
+import workspace.ObjectSaveData;
+import workspace.ObjectSpecification;
+import workspace.ProvenanceAction;
+import workspace.SaveObjectsParams;
+import workspace.WorkspaceClient;
+import workspace.WorkspaceIdentity;
 
 public class SpeciesTreeBuilderServerTest {
     private static AuthToken token = null;
@@ -76,85 +67,6 @@ public class SpeciesTreeBuilderServerTest {
         JsonServerSyslog.setStaticMlogFile(new File(config.get("scratch"), "test.log").getAbsolutePath());
         impl = new SpeciesTreeBuilderServer();
         impl.getConfig().put("public.genomes.ws", getPublicWsName());
-        /*
-        // Store test genome (real data)
-        FastaReader fr = new FastaReader(new File("test/data", "Shewanella_ANA_3_uid58347.fasta"));
-        Map<String, String> proteinIdToSeq = new LinkedHashMap<>();
-        for (Map.Entry<String, String> entry : fr.readAll().entrySet())
-            proteinIdToSeq.put(entry.getKey(), entry.getValue());
-        String genomeName = "Shewanella_ANA_3_uid58347";
-        String genomeObjId = genomeName + ".genome";
-        BuildTestGenomeClient btg = new BuildTestGenomeClient(new URL(System.getenv("SDK_CALLBACK_URL")), token);
-        btg.setIsInsecureHttpConnectionAllowed(true);
-        btg.prepareTestGenomeFromProteins(
-                new PrepareTestGenomeFromProteinsParams().withGenomeName(genomeName)
-                .withProteinIdToSequence(proteinIdToSeq)
-                .withOutputWorkspaceName(getWsName()).withOutputObjectName(genomeObjId));
-
-        // Sync genome objects (just fake wrappers with kbase-id as name)
-        List<ObjectSaveData> taxonObjects = new ArrayList<ObjectSaveData>();
-        Map<String, Object> taxon = new LinkedHashMap<String, Object>(4);
-        taxon.put("taxonomy_id", 1);
-        taxon.put("scientific_name", "xxx");
-        taxon.put("domain", "xxx");
-        taxonObjects.add(new ObjectSaveData().withName("fakeTaxon").withType("KBaseGenomeAnnotations.Taxon").withData(new UObject(taxon)));
-        wsClient.saveObjects(new SaveObjectsParams().withWorkspace(getWsName()).withObjects(taxonObjects));
-
-        String genomeWsType = "KBaseGenomes.Genome";
-        List<ObjectSaveData> objects = new ArrayList<ObjectSaveData>();
-        Map<String, String> genomeKbIdToName = new LinkedHashMap<>();
-        genomeKbIdToName.put("GCF_000146165.2", "Shewanella oneidensis MR-1");
-        genomeKbIdToName.put("GCF_000016585.1", "Shewanella putrefaciens CN-32");
-        genomeKbIdToName.put("GCF_000015185.1", "Shewanella sp. W3-18-1");
-        genomeKbIdToName.put("GCF_000018765.1", "Shewanella baltica OS195");
-        genomeKbIdToName.put("GCF_000017325.1", "Shewanella baltica OS185");
-        genomeKbIdToName.put("GCF_000021665.1", "Shewanella baltica OS223");
-        genomeKbIdToName.put("GCF_000014685.1", "Shewanella sp. MR-4");
-        genomeKbIdToName.put("GCF_000014665.1", "Shewanella sp. MR-7");
-        genomeKbIdToName.put("GCF_000215895.1", "Shewanella baltica OS117");
-        genomeKbIdToName.put("GCF_000178875.2", "Shewanella baltica OS678");
-        genomeKbIdToName.put("GCF_000203935.1", "Shewanella sp. ANA-3");
-        genomeKbIdToName.put("GCF_000282755.1", "Shewanella sp. POL2");
-        genomeKbIdToName.put("GCF_000169215.2", "Shewanella putrefaciens 200");
-        genomeKbIdToName.put("GCF_000015845.1", "Shewanella baltica OS155");
-        genomeKbIdToName.put("GCF_000231345.1", "Shewanella baltica OS625");
-        genomeKbIdToName.put("GCF_000485795.1", "Shewanella decolorationis S1201");
-        // Skipping next genome just to check we can handle lost references
-        genomeKbIdToName.put("GCF_000712635.2", "Shewanella xiamenensis");
-        genomeKbIdToName.put("GCF_001723195.1", "Shewanella xiamenensis");
-        genomeKbIdToName.put("GCF_002074855.1", "Shewanella xiamenensis");
-        // And several genomes for 
-        genomeKbIdToName.put("GCF_000519065.1", "Shewanella putrefaciens HRCR-6");
-        genomeKbIdToName.put("GCF_002157365.1", "Shewanella putrefaciens");
-        genomeKbIdToName.put("GCF_001591325.1", "Shewanella putrefaciens JCM 20190 = NBRC 3908");
-        genomeKbIdToName.put("GCF_001308045.1", "Shewanella sp. Sh95");
-        genomeKbIdToName.put("GCF_000217915.1", "Shewanella sp. HN-41");
-        genomeKbIdToName.put("GCF_000798835.1", "Shewanella sp. ZOR0012");
-        genomeKbIdToName.put("GCF_001401775.1", "Shewanella sp. P1-14-1");
-        genomeKbIdToName.put("GCF_000147735.2", "Shewanella baltica BA175");
-        genomeKbIdToName.put("GCF_000013765.1", "Shewanella baltica");
-        for (String kbId : genomeKbIdToName.keySet()) {
-            Map<String, Object> data = new LinkedHashMap<String, Object>(4);
-            data.put("id", kbId);
-            String name = genomeKbIdToName.get(kbId);
-            data.put("scientific_name", name);
-            data.put("domain", "Bacteria");
-            data.put("genetic_code", 11L);
-            data.put("taxonomy", StringUtils.join(name.split(" "), "; ") + name);
-            data.put("genome_tiers", new ArrayList<String>());
-            data.put("dna_size", new Integer(1));
-            data.put("num_contigs", new Integer(1));
-            data.put("molecule_type", "xxx");
-            data.put("source", "{}");
-            data.put("md5", "xxx");
-            data.put("gc_content", new Float(1.0));
-            data.put("features", new ArrayList<String>());
-            data.put("cdss", new ArrayList<String>());
-            data.put("taxon_ref", getWsName() + "/fakeTaxon" );
-            data.put("feature_counts", new HashMap<String, Integer>());
-            objects.add(new ObjectSaveData().withName(kbId).withType(genomeWsType).withData(new UObject(data)));
-        }
-        wsClient.saveObjects(new SaveObjectsParams().withWorkspace(getPublicWsName()).withObjects(objects));*/
     }
     
     private static String getWsName() throws Exception {
@@ -218,10 +130,8 @@ public class SpeciesTreeBuilderServerTest {
 
     @Test
     public void testConstructSpeciesTree() throws Exception {
-        String genomeName = "Shewanella_ANA_3_uid58347";
-        String genomeId = genomeName + ".genome";
         String spTreeId = "sptree.1";
-        String genomeRef = "7601/4/1";
+        String genomeRef = "ReferenceDataManager/GCF_001593245.1";
         String treeRef = impl.constructSpeciesTree(new ConstructSpeciesTreeParams()
                                                    .withNewGenomes(Arrays.asList(genomeRef))
                                                    .withOutWorkspace(getWsName())
@@ -258,27 +168,27 @@ public class SpeciesTreeBuilderServerTest {
         // new BuildGenomeSetFromTreeParams().withTreeRef(treeRef)
         // .withGenomesetRef(getWsName() + "/genomeset.1"), token, getContext());
         // Assert.assertNotNull(genomeSetRef);
+        // int genomesFound = impl.findCloseGenomes(
+        //        new FindCloseGenomesParams().withQueryGenome(genomeRef), token, getContext()).size();
+        // Assert.assertEquals(4, genomesFound);
+        // String taxPath = impl.guessTaxonomyPath(
+        //        new GuessTaxonomyPathParams().withQueryGenome(genomeRef), token, getContext());
+        // Assert.assertEquals("Shewanella; baltica;", taxPath);
         ///////////////////////////////////////////////////////////////////////////////////////////
-        int genomesFound = impl.findCloseGenomes(
-                new FindCloseGenomesParams().withQueryGenome(genomeRef), token, getContext()).size();
-        Assert.assertEquals(4, genomesFound);
-        String taxPath = impl.guessTaxonomyPath(
-                new GuessTaxonomyPathParams().withQueryGenome(genomeRef), token, getContext());
-        Assert.assertEquals("Shewanella; baltica;", taxPath);
     }
 
     @Test(expected=IllegalArgumentException.class)
     public void testNoSameName() throws Exception {
-        String genomeName = "Shewanella_ANA_3_uid58347";
         String spTreeId = "sptree.1";
-        String genomeRef = getWsName() + "/" + spTreeId;
+        String genomeRef = "ReferenceDataManager/GCF_001593245.1";
+        String genomeSetRef = getWsName() + "/" + spTreeId;
         String treeRef = impl.constructSpeciesTree(new ConstructSpeciesTreeParams()
                         .withNewGenomes(Arrays.asList(genomeRef))
                         .withOutWorkspace(getWsName())
                         .withOutTreeId(spTreeId)
                         .withUseRibosomalS9Only(0L)
                         .withNearestGenomeCount(20L)
-                        .withOutGenomesetRef(genomeRef)
+                        .withOutGenomesetRef(genomeSetRef)
                         .withCopyGenomes(1L)
                 , token, getContext());
     }
